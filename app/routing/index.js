b7c1ec24c9276d1a42798b9099934c3f39a9cf75
Peter Wu <23109224+PapaWuski@users.noreply.github.com>
2019-10-06 18:17:33 -0700

    setting up statics for routing with express.static

--- app/routing/index.js
+++ app/routing/index.js
@@ -1,3 +1,7 @@
 const express = require("express");
 const router = express.Router();
-const api = require("./apiRoutes.js")
+const api = require("./apiRoutes.js");
+
+router.use("/api", api);
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,12 +1,14 @@
-const express = require("express")
-const path = require("path")
+const express = require("express");
+const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static(path.join(__dirname,"app/public")));
+app.use(
+  express.static(path.join(__dirname, "app/public"), { extensions: ["html"] })
+);
 
-app.listen(PORT,() =>{
-    console.log("connected")
-})
+app.listen(PORT, () => {
+  console.log("connected");
+});
