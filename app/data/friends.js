9c90060f8d83d9e52666714a16e7078f6a1d3069
Peter Wu <23109224+PapaWuski@users.noreply.github.com>
2019-10-07 19:26:15 -0700

    added urls

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,27 +1,27 @@
 const data = [
   {
     name: "Tim",
-    url: "",
+    url: "https://vignette.wikia.nocookie.net/disgaea/images/2/22/D2_Prinny_Artwork.jpg/revision/latest?cb=20100321054625&path-prefix=en",
     score: [1, 2, 3, 4, 5, 1, 2, 3, 4, 5]
   },
   {
     name: "Timmy",
-    url: "",
+    url: "https://vignette.wikia.nocookie.net/disgaea/images/e/e4/PCIRBTH_Hero_Prinny.png/revision/latest?cb=20131026184413&path-prefix=en",
     score: [2, 3, 4, 5, 1, 3, 4, 2, 3, 5]
   },
   {
     name: "Jimmy",
-    url: "",
+    url: "https://static.giantbomb.com/uploads/original/0/495/896322-952784_20081212_screen037.jpg",
     score: [1, 5, 3, 4, 1, 5, 3, 2, 1, 4]
   },
   {
     name: "Jim",
-    url: "",
+    url: "https://cdn.imgbin.com/11/19/20/imgbin-disgaea-hour-of-darkness-prinny-can-i-really-be-the-hero-disgaea-infinite-disgaea-2-disgaea-5-others-Jga9kKv7ubU0gpF29qJCqC4hJ.jpg",
     score: [5, 4, 3, 2, 4, 5, 4, 3, 2, 1]
   },
   {
     name: "Steve",
-    url: "",
+    url: "https://stickershop.line-scdn.net/stickershop/v1/product/1006011/LINEStorePC/main.png;compress=true",
     score: [1, 5, 3, 2, 4, 5, 5, 5, 5, 1]
   }
 ];
